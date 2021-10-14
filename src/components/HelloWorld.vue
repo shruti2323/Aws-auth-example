986e28adf076dcac3a14fa8afccc5928f20bf59e
shruti2323 <shruti.kamble@extrapreneursindia.com>
2021-10-14 10:26:25 +0530

    test amplify POC

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,80 +1,80 @@
-<template>
-  <div class="hello">
-    <div v-if="!signedIn">
-      <!-- <amplify-authenticator></amplify-authenticator> -->
-      <input v-model="login" type="text" name="" placeholder="Login" ><br>
-      <input v-model="password" type="password" name="" placeholder="Password" ><br>
-      <button @click="signIn">Sign in</button>
+<template> 
+  <div class="hello"> 
+    <div v-if="!signedIn"> 
+      <!-- <amplify-authenticator></amplify-authenticator> --> 
+      <input v-model="login" type="text" name="" placeholder="Login" ><br> 
+      <input v-model="password" type="password" name="" placeholder="Password" ><br> 
+      <button @click="signIn">Sign in</button> 
     </div>
-    <div v-if="signedIn">
+    <div v-if="signedIn"> 
       <!-- <amplify-sign-out></amplify-sign-out> -->
-      <button @click="signOut">Sign Out</button>
+      <button @click="signOut">Sign Out</button> 
     </div>
 
   </div>
 </template>
 
 <script>
-import { Auth } from 'aws-amplify'
-import { AmplifyEventBus } from 'aws-amplify-vue';
-export default {
-  name: 'HelloWorld',
+import { Auth } from 'aws-amplify' 
+import { AmplifyEventBus } from 'aws-amplify-vue'; 
+export default { 
+  name: 'HelloWorld', 
   data() {
-    return {
+    return { 
       login: '',
       password: ''
       // signedIn: false
     }
   },
-  props: {
+  props: { 
     msg: String,
   },
   created(){
-    this.findUser();
+    this.findUser(); 
 
-    AmplifyEventBus.$on('authState', info => {
+    AmplifyEventBus.$on('authState', info => { 
       if(info === "signedIn") {
-        this.findUser();
+        this.findUser(); 
       } else {
-        this.$store.state.signedIn = false;
+        this.$store.state.signedIn = false; 
         // this.signedIn = false;
-        this.$store.state.user = null;
+        this.$store.state.user = null; 
       }
     });
   },
   computed: {
-    signedIn(){
-      return this.$store.state.signedIn;
+    signedIn(){ 
+      return this.$store.state.signedIn; 
     }
   },
-  methods: {
+  methods: { 
     signIn(){
-      Auth.signIn(this.login, this.password)
+      Auth.signIn(this.login, this.password) 
         .then(user =>{
-            this.$store.state.signedIn = !!user;
-            this.$store.state.user = user;
+            this.$store.state.signedIn = !!user; 
+            this.$store.state.user = user; 
         } )
-        .catch(err => console.log(err));
+        .catch(err => console.log(err)); 
     },
     signOut() {
       Auth.signOut()
-        .then(data =>{
-          this.$store.state.signedIn = !!data;
+        .then(data =>{ 
+          this.$store.state.signedIn = !!data; 
         } )
-        .catch(err => console.log(err));
+        .catch(err => console.log(err)); 
     },
     async findUser() {
       try {
-        const user = await Auth.currentAuthenticatedUser();
+        const user = await Auth.currentAuthenticatedUser(); 
         // this.signedIn = true;
-        this.$store.state.signedIn = true;
-        this.$store.state.user = user;
-        console.log(user);
+        this.$store.state.signedIn = true; 
+        this.$store.state.user = user; 
+        console.log(user); 
 
       } catch(err) {
-        // this.signedIn = false;
-        this.$store.state.signedIn = false;
-        this.$store.state.user = null;
+        // this.signedIn = false; 
+        this.$store.state.signedIn = false; 
+        this.$store.state.user = null; 
       }
     }
   }
@@ -83,22 +83,22 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-input {
+input { 
   padding: 16px;
-  margin: 10px;
+  margin: 10px; 
 }
 h3 {
-  margin: 40px 0 0;
+  margin: 40px 0 0; 
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+  list-style-type: none; 
+  padding: 0; 
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+  display: inline-block; 
+  margin: 0 10px; 
 }
 a {
-  color: #42b983;
+  color: #42b983; 
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -1,18 +1,18 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue' 
+import Vuex from 'vuex' 
 
-Vue.use(Vuex)
+Vue.use(Vuex) 
 
-export default new Vuex.Store({
-  state: {
-    user: null,
-    signedIn: false
+export default new Vuex.Store({ 
+  state: { 
+    user: null, 
+    signedIn: false 
+
+  }, 
+  mutations: { 
 
   },
-  mutations: {
-
-  },
-  actions: {
+  actions: { 
 
   }
 })
